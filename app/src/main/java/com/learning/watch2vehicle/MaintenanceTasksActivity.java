acaac4c35d87ca270c41711484c84120f0298c87
Mansoor Ali <omansoor@gmail.com>
2022-02-24 23:05:12 +0500

    Added Screens and icons for - maintenance tasks - charging task
    Icons are triggering a warning for low color contrast
    TODO: Adjust icons contrast

--- app/src/main/java/com/learning/watch2vehicle/MaintenanceTasksActivity.java
+++ app/src/main/java/com/learning/watch2vehicle/MaintenanceTasksActivity.java
@@ -2,38 +2,33 @@ package com.learning.watch2vehicle;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
-import android.content.Intent;
-
 import android.os.Bundle;
-import android.view.View;
 import android.widget.TextView;
-import com.learning.watch2vehicle.databinding.ActivityMainBinding;
+
+import com.learning.watch2vehicle.databinding.ActivityMaintenanceTasksBinding;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class MainActivity extends Activity {
+public class MaintenanceTasksActivity extends Activity {
+
+    private TextView mTextView;
+    private ActivityMaintenanceTasksBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        com.learning.watch2vehicle.databinding.ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+
+        binding = ActivityMaintenanceTasksBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        mTextView = binding.text;
         @SuppressLint("SimpleDateFormat") DateFormat dateFormatter = new SimpleDateFormat("EEE d hh:mm");
         dateFormatter.setLenient(false);
         Date today = new Date();
         String s =  dateFormatter.format(today);
         TextView mTextView = binding.text;
         mTextView.setText(s);
-    }
-
-    public void startChargeTasksActivity(View view) {
-        startActivity(new Intent(this, ChargeTasksActivity.class));
-    }
-
-    public void startMaintenanceTasksActivity(View view)
-    {
-        startActivity(new Intent(this,MaintenanceTasksActivity.class));
     }
 }
